837470fea868dadb2b10a8ca0293618a8917e38b
Johnny7c3 <johnny7c3@gmail.com>
2020-03-29 10:30:36 +0300

    Correction_5

--- src/main/java/ru/javamentor/preproject/controller/UserController.java
+++ src/main/java/ru/javamentor/preproject/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
     
     @GetMapping("/")
     public String homePage() {
-        return "redirect:/users";
+        return "redirect: /users";
     }
 
     @GetMapping("/users")
@@ -34,14 +34,14 @@ public class UserController {
     @GetMapping("/add")
     public ModelAndView addPage(ModelMap model) {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("/edit");
+        modelAndView.setViewName("edit");
         return modelAndView;
     }
 
     @PostMapping("/add")
     public ModelAndView addUser(@ModelAttribute("user") User user) {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("redirect:/users");
+        modelAndView.setViewName("redirect: /users");
         userService.add(user);
         return modelAndView;
     }
@@ -49,7 +49,7 @@ public class UserController {
     @GetMapping("/edit/{id}")
     public ModelAndView editUserPage(@PathVariable("id") int id) {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("/edit");
+        modelAndView.setViewName("edit");
         User user = userService.getUserById(id);
         modelAndView.addObject("user", user);
         modelAndView.addObject("id", id);
@@ -60,7 +60,7 @@ public class UserController {
     public ModelAndView editUser(@RequestParam(value="id") int id,
             @ModelAttribute("user") User user) {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("redirect:/users");
+        modelAndView.setViewName("redirect: /users");
         user.setId(id);
         userService.update(user);
         return modelAndView;
@@ -69,7 +69,7 @@ public class UserController {
     @GetMapping("/delete/{id}")
     public ModelAndView deleteUser(@PathVariable("id") int id) {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("redirect:/users");
+        modelAndView.setViewName("redirect: /users");
         User user = userService.getUserById(id);
         if (user != null) {
             userService.delete(user);
